140e9cc683b86e53d0d3ee1b98874f8be01c670c
wills-dev <ejioguvictor6@gmail.com>
2024-03-17 20:51:08 +0100

    signup function

--- frontend/src/hooks/useSignUpUser.jsx
+++ frontend/src/hooks/useSignUpUser.jsx
@@ -2,6 +2,8 @@ import { useState } from "react";
 
 import toast from "react-hot-toast";
 
+import { axiosInstance } from "../axiosInstance/baseUrl.js";
+
 export const useSignUpUser = () => {
   const [registerUser, setRegisterUser] = useState({
     firstName: "",
@@ -29,86 +31,126 @@ export const useSignUpUser = () => {
           color: "#fff",
         },
       });
+      return true;
     } else if (!registerUser?.lastName) {
-      return toast.error("Please enter last name", {
+      toast.error("Please enter last name", {
         duration: 4000,
         style: {
           background: "#f74764",
           color: "#fff",
         },
       });
+      return true;
     } else if (!registerUser?.userName) {
-      return toast.error("Please enter user name", {
+      toast.error("Please enter user name", {
         duration: 4000,
         style: {
           background: "#f74764",
           color: "#fff",
         },
       });
+      return true;
     } else if (
       !registerUser?.gender ||
       registerUser?.gender === "select gender"
     ) {
-      return toast.error("Please select gender", {
+      toast.error("Please select gender", {
         duration: 4000,
         style: {
           background: "#f74764",
           color: "#fff",
         },
       });
+      return true;
     } else if (!registerUser?.password) {
-      return toast.error("Please enter password", {
+      toast.error("Please enter password", {
         duration: 4000,
         style: {
           background: "#f74764",
           color: "#fff",
         },
       });
+      return true;
     } else if (registerUser?.password.length < 6) {
-      return toast.error("Password must be more than 6", {
+      toast.error("Password must be more than 6", {
         duration: 4000,
         style: {
           background: "#f74764",
           color: "#fff",
         },
       });
+      return true;
     } else if (
       !registerUser?.confrimPassword ||
       registerUser?.confrimPassword !== registerUser?.password
     ) {
-      return toast.error("Confirm password doesn't match password", {
+      toast.error("Confirm password doesn't match password", {
         duration: 4000,
         style: {
           background: "#f74764",
           color: "#fff",
         },
       });
+      return true;
+    } else {
+      return false;
     }
   };
 
   const handleSignUpUser = async (e) => {
     e.preventDefault();
-    handleRegistrationError();
+    const successMessage = handleRegistrationError();
     setLoading(true);
-    try {
-    } catch (error) {
-      console.log(error);
-      if (error?.message) {
-        toast.error(`${error.message}`, {
+    if (!successMessage) {
+      try {
+        const registrationDetails = {
+          firstName: registerUser.firstName,
+          lastName: registerUser.lastName,
+          userName: registerUser.userName,
+          gender: registerUser.gender,
+          password: registerUser.password,
+          confrimPassword: registerUser.confrimPassword,
+        };
+        const res = await fetch("/api/auth/signup", {
+          method: "POST",
+          headers: { "Content-Type": "application/json" },
+          body: JSON.stringify(registrationDetails),
+        });
+
+        const data = await res.json();
+        // const data = await axiosInstance.post(
+        //   "/auth/signup",
+        //   registrationDetails
+        // );
+        toast.success("You have successfully signed up to BUZZCONNECT", {
           duration: 4000,
           style: {
             background: "#f74764",
             color: "#fff",
           },
         });
-      } else {
-        toast.error(`Internal server error`, {
-          duration: 4000,
-          style: {
-            background: "#f74764",
-            color: "#fff",
-          },
-        });
+        setLoading(false);
+        console.log(data);
+      } catch (error) {
+        console.log(error);
+        setLoading(false);
+        if (error?.message) {
+          toast.error(`${error.message}`, {
+            duration: 4000,
+            style: {
+              background: "#f74764",
+              color: "#fff",
+            },
+          });
+        } else {
+          toast.error(`Internal server error`, {
+            duration: 4000,
+            style: {
+              background: "#f74764",
+              color: "#fff",
+            },
+          });
+        }
       }
     }
   };
