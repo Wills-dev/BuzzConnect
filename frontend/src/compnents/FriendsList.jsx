1bc747032dde766506d6b918c765df0d2801b899
wills-dev <ejioguvictor6@gmail.com>
2024-03-18 16:18:39 +0100

    integrated the login  function on the  frontend

--- frontend/src/compnents/FriendsList.jsx
+++ frontend/src/compnents/FriendsList.jsx
@@ -1,70 +1,38 @@
 import React from "react";
+import { useGetOtherUsers } from "../hooks/useGetOtherUsers";
 
 const FriendsList = () => {
+  const { loading, otherUsers } = useGetOtherUsers();
+
+  console.log(otherUsers);
   return (
     <div className="py-2 flex flex-col overflow-auto">
-      <>
-        <div className="flex gap-2 items-center hover:bg-pink-500 rounded px-2 py-1 cursor-pointer">
-          <div className="online avatar">
-            <div className="w-12 rounded-full h-12 flex justify-center items-center font-semibold bg-pink-100 border-pink-100 border-[1px]">
-              <div className="flex justify-center items-center h-full w-full text-gray-500">
-                VW
+      {otherUsers &&
+        otherUsers?.map((user) => (
+          <>
+            <div
+              className="flex gap-2 items-center hover:bg-pink-500 rounded px-2 py-1 cursor-pointer"
+              key={user._id}
+            >
+              <div className="online avatar">
+                <div className="w-12 rounded-full h-12 flex justify-center items-center font-semibold bg-pink-100 border-pink-100 border-[1px]">
+                  <div className="flex justify-center items-center h-full w-full text-gray-500">
+                    {user?.firstName?.charAt(0).toUpperCase()}
+                    {user?.lastName?.charAt(0).toUpperCase()}
+                  </div>
+                </div>
+              </div>
+              <div className="flex flex-col flex-1">
+                <div className="flex gap-3">
+                  <p className="font-semibold text-gray-200">
+                    {user?.firstName} {user?.lastName}
+                  </p>
+                </div>
               </div>
             </div>
-          </div>
-          <div className="flex flex-col flex-1">
-            <div className="flex gap-3">
-              <p className="font-semibold text-gray-200">Victor Wills</p>
-            </div>
-          </div>
-        </div>
-        <div className="divider my-0 py-0 h-1" />
-        <div className="flex gap-2 items-center hover:bg-pink-500 rounded px-2 py-1 cursor-pointer">
-          <div className="online avatar">
-            <div className="w-12 rounded-full h-12 flex justify-center items-center font-semibold bg-pink-100 border-pink-100 border-[1px]">
-              <div className="flex justify-center items-center h-full w-full text-gray-500">
-                VW
-              </div>
-            </div>
-          </div>
-          <div className="flex flex-col flex-1">
-            <div className="flex gap-3">
-              <p className="font-semibold text-gray-200">Victor Wills</p>
-            </div>
-          </div>
-        </div>
-        <div className="divider my-0 py-0 h-1" />
-        <div className="flex gap-2 items-center hover:bg-pink-500 rounded px-2 py-1 cursor-pointer">
-          <div className="online avatar">
-            <div className="w-12 rounded-full h-12 flex justify-center items-center font-semibold bg-pink-100 border-pink-100 border-[1px]">
-              <div className="flex justify-center items-center h-full w-full text-gray-500">
-                VW
-              </div>
-            </div>
-          </div>
-          <div className="flex flex-col flex-1">
-            <div className="flex gap-3">
-              <p className="font-semibold text-gray-200">Victor Wills</p>
-            </div>
-          </div>
-        </div>
-        <div className="divider my-0 py-0 h-1" />
-        <div className="flex gap-2 items-center hover:bg-pink-500 rounded px-2 py-1 cursor-pointer">
-          <div className="online avatar">
-            <div className="w-12 rounded-full h-12 flex justify-center items-center font-semibold bg-pink-100 border-pink-100 border-[1px]">
-              <div className="flex justify-center items-center h-full w-full text-gray-500">
-                VW
-              </div>
-            </div>
-          </div>
-          <div className="flex flex-col flex-1">
-            <div className="flex gap-3">
-              <p className="font-semibold text-gray-200">Victor Wills</p>
-            </div>
-          </div>
-        </div>
-        <div className="divider my-0 py-0 h-1" />
-      </>
+            <div className="divider my-0 py-0 h-1" />
+          </>
+        ))}
     </div>
   );
 };
---
+++ frontend/src/hooks/useGetOtherUsers.js
@@ -0,0 +1,59 @@
+import { useEffect, useState } from "react";
+
+import toast from "react-hot-toast";
+
+export const useGetOtherUsers = () => {
+  const [loading, setLoading] = useState();
+  const [otherUsers, setOtherUsers] = useState([]);
+
+  const getAllOtherUsers = async () => {
+    setLoading(true);
+    try {
+      const res = await fetch(`/api/users`);
+      const data = await res.json();
+      console.log(data);
+
+      if (data.error) {
+        throw new Error(data.error);
+      }
+      setOtherUsers(data);
+      setLoading(false);
+    } catch (error) {
+      setLoading(false);
+      if (error?.message) {
+        toast.error(`${error.message}`, {
+          duration: 4000,
+          style: {
+            background: "#f74764",
+            color: "#fff",
+          },
+        });
+      } else if (error?.error?.message) {
+        toast.error(`${error.error.message}`, {
+          duration: 4000,
+          style: {
+            background: "#f74764",
+            color: "#fff",
+          },
+        });
+      } else {
+        toast.error(`Internal server error`, {
+          duration: 4000,
+          style: {
+            background: "#f74764",
+            color: "#fff",
+          },
+        });
+      }
+    }
+  };
+
+  useEffect(() => {
+    getAllOtherUsers();
+  }, []);
+
+  return {
+    loading,
+    otherUsers,
+  };
+};
