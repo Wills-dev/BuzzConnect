a30636718bf947f24359c55a050de609acebafe3
wills-dev <ejioguvictor6@gmail.com>
2024-03-19 06:12:23 +0100

    integrated socket.io for backend

--- backend/server.js
+++ backend/server.js
@@ -7,8 +7,8 @@ import messageRoutes from "./routes/message.routes.js";
 import userRoutes from "./routes/user.routes.js";
 
 import connectToMongoDB from "./db/connectToMongoDb.js";
+import { app, server } from "./socket/socket.js";
 
-const app = express();
 const PORT = process.env.PORT || 5000;
 
 dotenv.config();
@@ -25,7 +25,7 @@ app.use("/api/users", userRoutes);
 //   res.send("Hello world! hhehhehhe");
 // });
 
-app.listen(PORT, () => {
+server.listen(PORT, () => {
   connectToMongoDB();
   console.log(`Server running on port ${PORT}`);
 });
---
+++ backend/socket/socket.js
@@ -0,0 +1,23 @@
+import { Server } from "socket.io";
+import http from "http";
+import express from "express";
+
+const app = express();
+
+const server = http.createServer(app);
+const io = new Server(server, {
+  cors: {
+    origin: ["http://127.0.0.1:5173"],
+    methods: ["GET", "POST"],
+  },
+});
+io.on("connection", (socket) => {
+  console.log("a user connected", socket.id);
+
+  //socket.on() is used to listen to the events. cann be used both on client and server side
+  socket.on("disconnect", () => {
+    console.log("user disconnect", socket.id);
+  });
+});
+
+export { app, io, server };
