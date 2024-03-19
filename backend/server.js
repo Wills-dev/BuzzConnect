38757716fefc90935c5521f297d027ae230305c6
wills-dev <ejioguvictor6@gmail.com>
2024-03-19 12:03:36 +0100

    Deployment

--- backend/server.js
+++ backend/server.js
@@ -7,7 +7,7 @@ import authRoutes from "./routes/auth.routes.js";
 import messageRoutes from "./routes/message.routes.js";
 import userRoutes from "./routes/user.routes.js";
 
-import connectToMongoDB from "./db/connectToMongoDb.js";
+import connectToMongoDB from "./db/connectToMongoDB.js";
 import { app, server } from "./socket/socket.js";
 
 const PORT = process.env.PORT || 5000;
